4b205670963eea7f7794c840a14efbf6b9977d4e
Srishty Mangutte <srishtymangutte@gmail.com>
2023-06-18 01:52:32 +0530

    feat: added mongoose

--- index.js
+++ index.js
@@ -1,50 +1,91 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.json());
 
-// In-memory data store for articles
-let articles = [];
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/blogDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+// Define the article schema
+const articleSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  views: { type: Number, default: 0 }
+});
+
+// Create the article model
+const Article = mongoose.model('Article', articleSchema);
 
 // Get all articles
 app.get('/articles', (req, res) => {
-  res.json({ articles: articles });
+  Article.find()
+    .then(articles => {
+      res.json({ articles: articles });
+    })
+    .catch(err => {
+      console.error('Error getting articles:', err);
+      res.sendStatus(500);
+    });
 });
 
 // Add a new article
 app.post('/articles', (req, res) => {
   const { title, content } = req.body;
-  const newArticle = { title: title, content: content, views: 0 };
-  articles.push(newArticle);
-  res.json(newArticle);
+  const newArticle = new Article({ title: title, content: content });
+
+  newArticle.save()
+    .then(article => {
+      res.json(article);
+    })
+    .catch(err => {
+      console.error('Error adding article:', err);
+      res.sendStatus(500);
+    });
 });
 
 // Update an article
 app.put('/articles/:id', (req, res) => {
-  const articleId = parseInt(req.params.id);
+  const articleId = req.params.id;
   const { title, content } = req.body;
 
-  const article = articles.find(article => articleId === article.id);
-  if (article) {
-    article.title = title;
-    article.content = content;
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
-  }
+  Article.findByIdAndUpdate(articleId, { title: title, content: content }, { new: true })
+    .then(article => {
+      if (article) {
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch(err => {
+      console.error('Error updating article:', err);
+      res.sendStatus(500);
+    });
 });
 
 // Remove an article
 app.delete('/articles/:id', (req, res) => {
-  const articleId = parseInt(req.params.id);
-  const articleIndex = articles.findIndex(article => articleId === article.id);
-  if (articleIndex !== -1) {
-    articles.splice(articleIndex, 1);
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
-  }
+  const articleId = req.params.id;
+
+  Article.findByIdAndRemove(articleId)
+    .then(article => {
+      if (article) {
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch(err => {
+      console.error('Error removing article:', err);
+      res.sendStatus(500);
+    });
 });
 
 app.listen(3000, () => {
